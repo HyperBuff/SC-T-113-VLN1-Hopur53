7bddc7ed23a054ae4f984ea13a63233d57979142
aleifsm <aleifsm@gmail.com>
2020-12-07 10:43:15 +0000

    new location

--- ui/LocationUI.py
+++ ui/LocationUI.py
@@ -1,60 +1,66 @@
-import math
-
 from logic.MainLogic import MainLogic
 from models.Location import Location
 
 from ui.PrinterUI import PrinterUI
-
+from ui.InputUI import InputUI
 
 class LocationUI:
 
     def __init__(self):
+        self.items_per_page = 10
+
         self.logic = MainLogic()
         self.printer = PrinterUI()
-        self.items_per_page = 10
+        self.input = InputUI()
+
         self.success_msg = ""
         self.warning_msg = ""
-    
 
+    # Prints out location's menu
     def menu(self):
         while True:
-            self.printer.header("Location Menu")
-            self.printer.print_options(['Create new location', 'Modify location information', 'View location information', 'View opening hour overview'])
+            self.printer.header("locations Menu")
+            self.printer.print_options(['Create an location', 'View locations'])
             self.printer.new_line(2)
             self.printer.print_fail("Press q to go back")
             self.print_msg()
+
             action = input("Choose an option: ").lower()
+            
             if action == '1':
                 if self.create():
                     self.success_msg = "New location has been created"
-                    self.view()
+                    self.view(True)
             elif action == '2':
-                self.edit(id)
-            elif action == '3':
                 self.view()
-            elif action == '4':
-                self.opening_hours()
             elif action == 'q':
                 break
             else:
                 self.warning_msg = "Please select available option"
 
-    
-    # Prints out all locations
-    def view(self, current_page = 1):
-
+    # Prints out all location
+    def view(self, created = False):
+        current_page = 1
         while True:   
             locations = self.logic.get_all_locations()
             locations_count = len(locations)
-            last_page = math.ceil(locations_count / self.items_per_page)
-            self.printer.header("View locations")
+            last_page = int(locations_count / self.items_per_page) + (locations_count % self.items_per_page > 0)
+            if current_page > last_page:
+                current_page = last_page
+            if created == True:
+                current_page = last_page
+                created = False
             start = (current_page - 1) * self.items_per_page
             end = start + 10 if not current_page == last_page else locations_count
+
+            self.printer.header("View locations")
             self.print_locations(locations, start, end, current_page, last_page)
             self.printer.new_line()
             self.printer.print_fail("Press q to go back")
             self.print_msg()
+
             action = input("(N)ext page / (P)revious page / (S)elect location: ").lower()
+
             if action == 'q':
                 break
             elif action == 'n' or action == "next":
@@ -73,19 +79,18 @@ class LocationUI:
                 location_id = input("Select location by ID: ")
                 location = self.logic.get_location_by_id(location_id)
                 if location is None:
-                    self.warning_msg = "Location not found"
+                    self.warning_msg = "location not found"
                 else:
                     self.select_location(location_id)
             else:
                 self.warning_msg = "Please select available option"
-
 
     # Prints out single location
     def select_location(self, location_id):
         while True:
             location = self.logic.get_location_by_id(location_id)
             self.printer.header("View location")
-            print("Country:\t\t\t\t{}\nAirport:\t\t\t\t{}\nPhone number:\t\t\t\t{}\nOpening hours:\t\t\t\t{}ID:\t\t\t\t{}\n".format(location.country, location.airport, location.phone, location.opening_hours, location_id))
+            print("ID:\t\t\t\t{}\nRole:\t\t\t\t{}\nName:\t\t\t\t{}\nEmail:\t\t\t\t{}\nSocial security number:\t\t{}\nMobile phone:\t\t\t{}\nHome phone:\t\t\t{}\nAddress:\t\t\t{}\nPostal code:\t\t\t{}\n".format(location_id, location.role, location.name, location.email, location.ssn, location.phone, location.homephone, location.address, location.postal))
             self.printer.new_line()
             self.printer.print_fail("Press q to go back")
             self.print_msg()
@@ -95,22 +100,23 @@ class LocationUI:
             elif action == 'e' or action == 'edit':
                 self.edit(location_id)
             elif action == 'd' or action == 'delete':
-                self.delete(location_id)
-                self.success_msg = "Location has been deleted"
-                break
+                if self.delete(location_id):
+                    self.success_msg = "Location has been deleted"
+                    break
             else:
                 self.warning_msg = "Please select available option"
             
-
-    # Prints out table of locations
-    def print_locations(self, location, start, end, current_page, last_page):
-        print("|{:^40}|{:<30}|{:<35}|{:<20}|{:<20}|".format("ID", "Country", "Airport", "Phone number", "Opening hours"))
-        print('-' * 216)
-        for i in range(start, end):
-            print("|{:^40}|{:<30}|{:<35}|{:<20}|{:<20}|".format(location[i].id, location[i].country, location[i].airport, location[i].phone, location[i].opening_hours))
-        print("{:^216}".format("Page {} of {}".format(current_page, last_page)))
-        self.printer.new_line()
-
+    # Prints out table of location
+    def print_locations(self, locations, start, end, current_page, last_page):
+        if len(locations) > 0:
+            print("|{:^6}|{:^15}|{:^30}|{:^40}|{:^30}|{:^20}|{:^20}|{:^30}|{:^15}|".format("ID", "Role", "Name", "Email", "Social security number", "Mobile phone", "Home phone", "Address", "Postal code"))
+            print('-' * 216)
+            for i in range(start, end):
+                print("|{:^6}|{:<15}|{:<30}|{:<40}|{:<30}|{:<20}|{:<20}|{:<30}|{:<15}|".format(locations[i].id, locations[i].role, locations[i].name, locations[i].email, locations[i].ssn, locations[i].phone, locations[i].homephone, locations[i].address, locations[i].postal))
+            print("{:^216}".format("Page {} of {}".format(current_page, last_page)))
+            self.printer.new_line()
+        else:
+            self.warning_msg = "No locations found"
 
     # Create location
     def create(self):
@@ -118,128 +124,61 @@ class LocationUI:
         self.printer.new_line()
         self.printer.print_fail("Press q to go back")
         self.printer.new_line()
-        print("Enter location details:")
-        while True:
-            country = input("\tEnter country name: ")
-            if country == 'q':
-                return
-            if len(country) < 1:
-                self.printer.print_warning("Country name must be at least 1 character")
-            else:
-                break
-        while True:
-            airport = input("\tEnter airport: ")
-            if airport == 'q':
-                return
-            if len(airport) < 1:
-                self.printer.print_warning("Airport name must be at least 1 character")
-            else:
-                break
-        while True:
-            phone = input("\tEnter airport's phone number: ")
-            if phone == 'q':
-                return
-            if len(phone) < 1:
-                self.printer.print_warning("Phone number must be at least 1 character")
-            elif not self.logic.is_phone_number_valid(phone):
-                self.printer.print_warning("Phone number is not valid")
-            else:
-                break
-        while True:
-            opening_hours = input("\tEnter opening hours: ")
-            if opening_hours == 'q':
-                return
-            if len(opening_hours) < 1:
-                self.printer.print_warning("Address must been at least 1 character")
-            else:
-                break
+        try:
+            country = self.input.get_input("country")
+            airport = self.input.get_input("airport")
+            phone = self.input.get_input("phone", ["phone"])
+            hours = self.input.get_input("hours")
 
-        new_location = Location(country, airport, phone, opening_hours)
-        self.logic.create_location(new_location) 
-        return True         
- 
-
+            new_location = Location(country, airport, phone, hours)
+            self.logic.create_location(new_location)
+            return True
+        except ValueError:
+            return False
+       
     # Edit location
     def edit(self, id):
         while True:
             updates = {}
             self.printer.header("Edit location")
-            self.printer.print_options(['Change country', 'Change airport', 'Change phone', 'Change opening hours'])
+            self.printer.print_options(['Change role', 'Change email', 'Change mobile phone', 'Change home phone', 'Edit address', 'Edit postal code'])
             self.printer.new_line(2)
             self.printer.print_fail("Press q to go back")
             self.print_msg()
             action = input("Choose an option: ").lower()
             if action == 'q':
                 break
-            if action == '1':
-                while True:
-                    country = input("\tEnter country: ")
-                    if country == 'q':
-                        break
-                    if len(country) < 1:
-                        self.printer.print_warning("Country name must be at least 1 character")
-                    else:
-                        updates["country"] = country
+            elif action == '1':
+                try:
+                    country = self.input.get_input("country")
+                    updates["country"] = country
+                except ValueError:
+                    break
             elif action == '2':
-                while True:
-                    airport = input("\tEnter airport: ")
-                    if airport == 'q':
-                        break
-                    if len(airport) < 1:
-                        self.printer.print_warning("Airport must be at least 1 character")
-                    else:
-                        updates["airport"] = airport
-                        break
+                try:
+                    airport = self.input.get_input("airport")
+                    updates["airport"] = airport
+                except ValueError:
+                    break
             elif action == '3':
-                while True:
-                    phone = input("\tEnter airport's phone number: ")
-                    if phone == 'q':
-                        break
-                    if len(phone) < 1:
-                        self.printer.print_warning("Phone must be at least 1 character")
-                    elif not self.logic.is_phone_number_valid(phone):
-                        self.printer.print_warning("Phone number is not valid")
-                    else:
-                        updates["phone"] = phone
-                        break
+                try:
+                    phone = self.input.get_input("phone", ["phone"])
+                    updates["phone"] = phone
+                except ValueError:
+                    break
             elif action == '4':
-                while True:
-                    opening_hours = input("\tEnter opening hours: ")
-                    if phone == 'q':
-                        break
-                    if len(opening_hours) < 1:
-                        self.printer.print_warning("Opening hours must be at least 1 character")
-                    else:
-                        updates["opening hours"] = opening_hours
-                        break
-            
-            self.logic.update_employee(id, updates)
+                try:
+                    hours = self.input.get_input("hours")
+                    updates["hours"] = hours
+                except ValueError:
+                    break
+            else:
+                self.warning_msg = "Please select available option"
 
-            keys = list(updates.keys())
+            if(len(updates) > 0):
+                self.logic.update_location(id, updates)
+                self.success_msg = "{} has been modified".format(list(updates.keys())[0].capitalize())
 
-            if(len(keys) > 0):
-                col = keys[0]
-                self.success_msg = "{} has been modified".format(col.capitalize())
-
-
-    def opening_hours(self, current_page = 1):  #Eftir að gera
-        location = self.logic.get_all_locations()
-        location_count = len(location)
-        last_page = math.ceil(location_count / self.items_per_page)
-        start = (current_page - 1) * self.items_per_page
-        end = start + 10 if not current_page == last_page else location_count
-        self.printer.header("Opening hour overview")
-        self.printer.new_line()
-        self.printer.print_fail("Press q to go back")
-        self.printer.new_line()
-        for i in range(start, end):
-            print("\t{}\t\t{}".format(location[i].country, location[i].hours))
-        self.printer.new_line()
-        self.printer.print_fail("Press q to go back")
-        self.print_msg()
-
-
-    
     # Delete location
     def delete(self, id):  
         confirmation = input("Are you sure you want to delete this location? (\33[;32mY\33[;0m/\33[;31mN\33[;0m): ").lower()
@@ -248,7 +187,6 @@ class LocationUI:
             return True
         return False
 
-    
     # Outputs warnings and success messages
     def print_msg(self):
         if not self.warning_msg == "":
@@ -259,8 +197,3 @@ class LocationUI:
             self.success_msg = ""
         else:
             self.printer.new_line()
-
-
-    
-
-
